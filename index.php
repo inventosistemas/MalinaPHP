0e2f322e97cd923b61a6a60e0c4633d170e0a6f0
rafaelserie <rafael@seriedesign.com.br>
2018-01-15 16:33:45 -0200

    Ajuste mask

--- index.php
+++ index.php
@@ -84,7 +84,7 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
 
 	<!-- Scripts -->
-	<script src="/javascripts/jquery.maskedinput.js"></script>
+	<script src="/javascripts/jquery.mask.js"></script>
 	<script type="text/javascript">
 		function obterBearer() {
 			$('#resultBearer').html('Autenticando...');
